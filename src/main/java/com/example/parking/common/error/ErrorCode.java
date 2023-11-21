59c23375101b5e82c65ff7182a26fd6033974016
Noah Kim <noah7203@berkeley.edu>
2023-11-21 11:55:42 +0900

    [FEAT] 응답형식 예외처리

---
+++ src/main/java/com/example/parking/common/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.parking.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements ErrorCodeIfs{
+    OK(200, 200, "Success"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "Bad Request "),
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "Server Error"),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "Null Point")
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String description;
+}
