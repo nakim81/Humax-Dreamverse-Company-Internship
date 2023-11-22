ce9aa0b78e26bb3c4c3c1588824bf7c08a781b23
Noah Kim <noah7203@berkeley.edu>
2023-11-22 15:02:03 +0900

    [FEAT][C-03]검색 내역 삭제 기증 구현

--- src/main/java/com/example/parking/controller/SearchHistoryApiController.java
+++ src/main/java/com/example/parking/controller/SearchHistoryApiController.java
@@ -4,6 +4,7 @@ import com.example.parking.common.api.Api;
 import com.example.parking.dto.SearchHistoryDto;
 import com.example.parking.service.SearchHistoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,14 @@ public class SearchHistoryApiController {
     ){
         var response = searchHistoryService.getSearchHistory(userId);
         return Api.OK(response);
+    }
+
+    @DeleteMapping("/user/{userId}/{historyId}")
+    public Api<Void> deleteSearchHistory(
+            @PathVariable("userId") Integer userId,
+            @PathVariable("historyId") Integer historyId
+    ){
+        searchHistoryService.deleteSearchHistory(userId, historyId);
+        return Api.OK(null);
     }
 }
