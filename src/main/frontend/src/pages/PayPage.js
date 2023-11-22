15724f236fc0b52d156f2678fa8b250f72aefe3f
진주현 <jinju0405@naver.com>
2023-11-22 17:34:03 +0900

    Revert "Revert "[FEAT] frontend 초기 설정""
    This reverts commit a85a52abc3fc837d777482d9c2791461b13f9dc7.

---
+++ src/main/frontend/src/pages/PayPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PayPage = () => {
+    return (
+        <>
+            <div>
+                <h1>PayPage</h1>
+            </div>
+        </>
+    );
+};
+
+export default PayPage;
