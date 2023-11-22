9e82ab203f02e3a7eea21f773fb3d9f9cf531341
진주현 <jinju0405@naver.com>
2023-11-22 17:26:42 +0900

    [FEAT] frontend 초기 설정

---
+++ src/main/frontend/src/pages/ParkinglotPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ParkinglotPage = () => {
+    return (
+        <>
+            <div>
+                <h1>ParkinglotPage</h1>
+            </div>
+        </>
+    );
+};
+
+export default ParkinglotPage;
