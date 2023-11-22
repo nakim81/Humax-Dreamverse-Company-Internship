b44552ab8f97fffa76fd6e3c9955d01e429eb764
Noah Kim <noah7203@berkeley.edu>
2023-11-22 15:34:39 +0900

    [STYLE] 필터 로거 출력 스타일 수정

--- src/main/java/com/example/parking/filter/LoggerFilter.java
+++ src/main/java/com/example/parking/filter/LoggerFilter.java
@@ -20,47 +20,56 @@ public class LoggerFilter implements Filter {
 
         chain.doFilter(req, res); // objects passed to controllers or any further steps are wrapped as ContentCachingWrapper
 
-        // request info
-        var headerNames = req.getHeaderNames();
-        var headerValues = new StringBuilder();
-
-        headerNames.asIterator().forEachRemaining(headerKey -> {
-            var headerValue = req.getHeader(headerKey);
-
-            // authorization-token : ???, user-agent : ???
-            headerValues.append("[")
-                    .append(headerKey)
-                    .append(" : ")
-                    .append(headerValue)
-                    .append("]")
-            ;
-
-        });
         var requestBody = new String(req.getContentAsByteArray());
         var uri = req.getRequestURI();
         var method = req.getMethod();
 
-        log.info(">>>>> uri : {}, method : {}, header : {}, body : {}",uri, method, headerValues, requestBody);
+        // Ignore Swagger UI requests
+        if (!uri.startsWith("/swagger-ui.html") && !uri.startsWith("/webjars/") &&
+                !uri.startsWith("/swagger-resources") && !uri.startsWith("/v2/api-docs")) {
 
-        // response info
-        var responseHeaderValues = new StringBuilder();
+            // ... existing logging code ...
+            // request info
+            var headerNames = req.getHeaderNames();
+            var headerValues = new StringBuilder();
 
-        res.getHeaderNames().forEach(headerKey -> {
-            var headerValue = res.getHeader(headerKey);
+            headerNames.asIterator().forEachRemaining(headerKey -> {
+                var headerValue = req.getHeader(headerKey);
 
-            responseHeaderValues
-                    .append("[")
-                    .append(headerKey)
-                    .append(" : ")
-                    .append(headerValue)
-                    .append("]");
-        });
+                // authorization-token : ???, user-agent : ???
+                headerValues.append("[")
+                        .append(headerKey)
+                        .append(" : ")
+                        .append(headerValue)
+                        .append("]")
+                ;
 
-        var responseBody = new String(res.getContentAsByteArray());
+            });
 
-        log.info("<<<<< uri : {}, method : {}, header : {}, body : {}", uri, method, responseHeaderValues, responseBody);
+            log.info(">>>>> uri : {}, method : {}, header : {}, body : {}",uri, method, headerValues, requestBody);
 
-        res.copyBodyToResponse();
+            // response info
+            var responseHeaderValues = new StringBuilder();
+
+            res.getHeaderNames().forEach(headerKey -> {
+                var headerValue = res.getHeader(headerKey);
+
+                responseHeaderValues
+                        .append("[")
+                        .append(headerKey)
+                        .append(" : ")
+                        .append(headerValue)
+                        .append("]");
+            });
+
+            var responseBody = new String(res.getContentAsByteArray());
+
+            log.info("<<<<< uri : {}, method : {}, header : {}, body : {}", uri, method, responseHeaderValues, responseBody);
+
+            res.copyBodyToResponse();
+
+        }
+
     }
 }
 
