724b0a714bf6b30fb622783f4a585346ad371f79
mii2026 <mii2026@skku.edu>
2023-11-23 13:18:08 +0900

    [B-05] 주차장 예약 상태 변경 구현

--- src/main/java/com/example/parking/repository/BookRepository.java
+++ src/main/java/com/example/parking/repository/BookRepository.java
@@ -5,9 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     @Query(value = "select b from Book b left join fetch b.user where b.bookId=:bookId")
     public Optional<Book> findByIDWithUser(@Param("bookId") Integer bookId);
+
+    @Query(value = "select b from Book b where b.state='이용대기' and b.startTime<=:currentTime")
+    public List<Book> findChangeStateToUsing(@Param("currentTime")LocalDateTime currentTime);
+
+    @Query(value = "select b from Book b where b.state='이용중' and b.endTime<=:currentTime")
+    public List<Book> findChangeStateToFinish(@Param("currentTime")LocalDateTime currentTime);
 }
