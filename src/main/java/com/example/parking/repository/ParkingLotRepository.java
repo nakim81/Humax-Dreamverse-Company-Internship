0fe921cd288340137ba9b1e4b57ce3ddf8a7614c
Noah Kim <noah7203@berkeley.edu>
2023-11-24 11:52:01 +0900

    [FIX] 리파지토리 기본키 데이터 타입 변경

--- src/main/java/com/example/parking/repository/ParkingLotRepository.java
+++ src/main/java/com/example/parking/repository/ParkingLotRepository.java
@@ -1,6 +1,8 @@
 package com.example.parking.repository;
 
 import com.example.parking.entity.Parkinglot;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,13 +11,17 @@ import java.util.List;
 
 import java.util.Optional;
 
-public interface ParkingLotRepository extends JpaRepository<Parkinglot, String> {
+public interface ParkingLotRepository extends JpaRepository<Parkinglot, Long> {
 
     public Optional<Parkinglot> findByName(String name);
 
-    @Query(value = "SELECT *, ( 6371 * acos( cos( radians(:latitude) ) * cos( radians( lat ) ) * cos( radians( lon ) - radians(:longitude) ) + sin( radians(:latitude) ) * sin(radians(lat)) ) ) AS distance FROM parking_lot HAVING distance < 25 ORDER BY distance LIMIT 0 , 15", nativeQuery = true)
-    List<Parkinglot> findNearestParkingLots(@Param("latitude") double latitude, @Param("longitude") double longitude);
+    Page<Parkinglot> findAll(Pageable pageable);
+
+    @Query(value = "SELECT *, ST_Distance_Sphere(point(lon, lat), point(:longitude, :latitude)) as distance FROM parking_lot HAVING distance < :radius * 1000 ORDER BY distance LIMIT 15", nativeQuery = true)
+    List<Parkinglot> findNearestParkingLots(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("radius") double radius);
+
 
     @Query(value = "SELECT p FROM Parkinglot p WHERE p.name LIKE CONCAT('%', :keyword, '%') OR p.address LIKE CONCAT('%', :keyword, '%')")
-    List<Parkinglot> findByNameOrAddressContains(@Param("keyword") String keyword);
+    Page<Parkinglot> findByNameOrAddressContains(@Param("keyword") String keyword, Pageable pageable);
+
 }
