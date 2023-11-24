f463821ed5332471b9d01b35994d2b068e48f220
진주현 <jinju0405@naver.com>
2023-11-24 11:34:56 +0900

    [REFACT] CarPage css 수정

--- src/main/frontend/src/pages/CarPage.js
+++ src/main/frontend/src/pages/CarPage.js
@@ -7,34 +7,70 @@ const CarPage = () => {
 
     const {userId} = useParams();
     const [carData, setCarData] = useState([]);
+    const [carUpdate, setCarUpdate] = useState(false);
+    const [updatedCarName, setUpdatedCarName] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(`http://localhost:8080/user/${userId}/car`)
                 setCarData(res.data)
+                console.log(res.data)
                 } catch (err) {
                 console.error('car data error', err)
             }
         }
 
         fetchData();
-    }, [userId, carData]);
+    }, [userId]);
+
+    const handleUpdateBtnClick = () => {
+        setCarUpdate((carUpdate) => !carUpdate);
+    }
+
+    const handleNameChange = (e) => {
+        setUpdatedCarName(e.target.value);
+    }
 
     return (
         <>
-            <div>
-                <h1>CarPage</h1>
-                <p>사용자 ID: {userId}</p>
-                {carData.map((car, index) => (
-                    <div key={index}>
-                        <div className="carContainer">
-                            <p>차량 이름: {car.carName}</p>
-                            <p>차량 번호: {car.carNumber}</p>
+            <h1>CarPage</h1>
+            <p>사용자 ID: {userId}</p>
+            {carData.map((car, index) => (
+                <div key={index}>
+                    <div className="carContainer">
+                        {carUpdate ? (
+                            <div className="carUpdateContainer">
+                                <input
+                                    type="text"
+                                    value={updatedCarName}
+                                    onChange={handleNameChange}
+                                    placeholder="차량 이름을 입력하세요."
+                                    className="carNameInput"
+                                />
+                                <input
+                                    type="text"
+                                    value={car.carNumber}
+                                    placeholder="차량 번호를 입력하세요."
+                                    className="carNameInput"
+                                />
+                            </div>
+                        ) : (
+                            <div className="carTextContainer">
+                                <p className="carNameText">{car.carName}</p>
+                                <p className="carNumberText">{car.carNumber}</p>
+                            </div>
+                        )
+                        }
+                        <div className="carBtnContainer">
+                            <button onClick={handleUpdateBtnClick} className="updateBtn">
+                                수정
+                            </button>
+                            <button className="deleteBtn">삭제</button>
                         </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </>
     );
 };
