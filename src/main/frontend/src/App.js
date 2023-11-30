9675c3811332b41bf636bbcdddb3c17b422397cc
Raindrop <ksu864@gmail.com>
2023-11-30 13:18:57 +0900

    [FEAT] 사용자 결제 수단 FE 기능 개발

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,6 +3,7 @@ import CarPage from "./pages/CarPage";
 import Home from "./pages/Home";
 import UserPage from "./pages/UserPage";
 import BookPage from "./pages/BookPage";
+import PayPage from "./pages/PayPage";
 import ParkinglotPage from "./pages/ParkinglotPage";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
             <Route element={<UserPage />} path="/user/:userId"/>
             <Route element={<BookPage />} path="/user/:userId/book"/>
             <Route element={<CarPage />} path="/user/:userId/car"/>
-            <Route element={<BookPage />} path="/user/:userId/pay"/>
+            <Route element={<PayPage />} path="/user/:userId/pay"/>
             <Route element={<ParkinglotPage />} path="/user/:userId/parkinglot"/>
         </Routes>
       </>
